b98ddd648e122bb50a0138a45589829c8ebc2cb0
kamal hamzah <kamalhamzah.sw@gmail.com>
2026-03-17 14:51:01 +0700

    Pertemuan 5 bagian latihan

---
+++ Pertemuan5/Pajak/src/Pajak.java
@@ -0,0 +1,11 @@
+/* File         : Pajak.java
+ * Deskripsi    : Program penggunaan Interface. Pembuatan Interface Pajak
+ * Pembuat      : Muhammad Kamal Hamzah - 24060124130056
+ * Lab          : D1
+ */
+
+public interface Pajak {
+    /* Method */
+    // Konstruktor
+    public double hitungPajak();
+}
