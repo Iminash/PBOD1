3315c9168c99b57a548ab0bf3ed905b10eadcd7d
kamal hamzah <kamalhamzah.sw@gmail.com>
2026-03-17 14:37:05 +0700

    Pertemuan 5 bagian bangun datar

---
+++ Pertemuan5/Bangun Datar/src/IResize.java
@@ -0,0 +1,11 @@
+/* Nama File : IResize.java
+ * Deskripsi : Pembuatan Interface IResize
+ * Pembuat : Muhammad Kamal Hamzah / 24060124130056
+ * Tanggal : 17/03/2026
+ */
+
+public interface IResize {
+    public void zoomIn();
+    public void zoomOut();
+    public void zoom(int percent);
+}
