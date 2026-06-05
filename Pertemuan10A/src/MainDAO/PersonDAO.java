3815a9bcc42c30bdb16c55da2a0972b5fc68a7c8
kamal hamzah <kamalhamzah.sw@gmail.com>
2026-06-05 23:00:04 +0700

    Pertemuan 10 file langsung dari netbeans

---
+++ Pertemuan10A/src/MainDAO/PersonDAO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainDAO;
+
+/**
+ *
+ * @author imina
+ */
+/**
+ * File : PersonDAO.java
+ * Deskripsi : interface untuk person access object
+ */
+public interface PersonDAO{
+    public void savePerson(Person p) throws Exception;
+}
+
